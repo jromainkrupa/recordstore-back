d7544c43327feacf48a08afa0ee2a34f1e45f238
jeanromain KRUPA <jkrupa@hotmail.fr>
2020-09-22 16:58:01 +0200

    created a bunch of controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   namespace :api do
     namespace :v1 do
       resources :records
@@ -7,6 +8,11 @@ Rails.application.routes.draw do
   end
 
   root to: 'home#index'
+
+  post 'refresh', controller: :refresh, action: :create
+  post 'signin', controller: :signin, action: :create
+  post 'signup', controller: :signup, action: :create
+  delete 'signin', controller: :signin, action: :destroy
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
