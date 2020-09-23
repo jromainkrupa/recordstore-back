8ba075a254f3b687b0a32394c6c0ce32155ce08e
jeanromain KRUPA <jkrupa@hotmail.fr>
2020-09-23 20:48:37 +0200

    vue views

--- recordsstore-front/src/router/index.js
+++ recordsstore-front/src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Signin from '@/components/Signin.vue'
+import Signup from '@/components/Signup.vue'
+
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'Signin',
+      component: Signin
     },
+    {
+      path: '/Signup',
+      name: 'Signup',
+      component: Signup
+    }
   ],
 });
